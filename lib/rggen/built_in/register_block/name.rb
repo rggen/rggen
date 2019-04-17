75890b43a6ffc4f9e766566bb4cd904451c58637
taichi <taichi730@gmail.com>
2019-04-18 01:23:17 +0900

    added register_block/name

---
+++ lib/rggen/built_in/register_block/name.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+RgGen.define_simple_feature(:register_block, :name) do
+  register_map do
+    property :name
+
+    ignore_empty_value false
+    input_pattern /(?<name>#{variable_name})/
+
+    build do |value|
+      @name =
+        if pattern_matched?
+          match_data[:name]
+        else
+          error "illegal input value for register block name: #{value.inspect}"
+        end
+      duplicated_name? && (
+        error "duplicated register block name: #{name}"
+      )
+    end
+
+    private
+
+    def duplicated_name?
+      register_map
+        .register_blocks
+        .any? { |register_block| register_block.name == name }
+    end
+  end
+end
