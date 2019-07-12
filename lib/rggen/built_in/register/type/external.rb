694beb5b36ba1d3279a355535082f73c416c4941
taichi <taichi730@gmail.com>
2019-07-12 16:34:02 +0900

    implemented RTL generation for external register type

--- lib/rggen/built_in/register/type/external.rb
+++ lib/rggen/built_in/register/type/external.rb
@@ -13,4 +13,105 @@ RgGen.define_list_item_feature(:register, :type, :external) do
       end
     end
   end
+
+  sv_rtl do
+    build do
+      if configuration.fold_sv_interface_port?
+        interface_port :register_block, :bus_if, {
+          name: "#{register.name}_bus_if",
+          interface_type: 'rggen_bus_if',
+          modport: 'master'
+        }
+      else
+        output :register_block, :valid, {
+          name: "o_#{register.name}_valid",
+          data_type: :logic,
+          width: 1
+        }
+        output :register_block, :address, {
+          name: "o_#{register.name}_address",
+          data_type: :logic,
+          width: address_width
+        }
+        output :register_block, :write, {
+          name: "o_#{register.name}_write",
+          data_type: :logic,
+          width: 1
+        }
+        output :register_block, :write_data, {
+          name: "o_#{register.name}_data",
+          data_type: :logic,
+          width: bus_width
+        }
+        output :register_block, :strobe, {
+          name: "o_#{register.name}_strobe",
+          data_type: :logic,
+          width: byte_width
+        }
+        input :register_block, :ready, {
+          name: "i_#{register.name}_ready",
+          data_type: :logic,
+          width: 1
+        }
+        input :register_block, :status, {
+          name: "i_#{register.name}_status",
+          data_type: :logic,
+          width: 2
+        }
+        input :register_block, :read_data, {
+          name: "i_#{register.name}_data",
+          data_type: :logic,
+          width: bus_width
+        }
+        interface :register, :bus_if, {
+          name: 'bus_if',
+          interface_type: 'rggen_bus_if',
+          parameter_values: [address_width, bus_width],
+          variables: [
+            'valid', 'address', 'write', 'write_data', 'strobe',
+            'ready', 'status', 'read_data'
+          ]
+        }
+      end
+    end
+
+    main_code :register do |code|
+      code << process_template
+      bus_if_connections(code) unless configuration.fold_sv_interface_port?
+    end
+
+    private
+
+    def address_width
+      register_block.local_address_width
+    end
+
+    def bus_width
+      configuration.bus_width
+    end
+
+    def byte_width
+      configuration.byte_width
+    end
+
+    def start_address
+      hex(register.offset_address, address_width)
+    end
+
+    def end_address
+      address = register.offset_address + register.byte_size - 1
+      hex(address, address_width)
+    end
+
+    def bus_if_connections(code)
+      code << assign(valid, bus_if.valid) << nl
+      code << assign(address, bus_if.address) << nl
+      code << assign(write, bus_if.write) << nl
+      code << assign(write_data, bus_if.write_data) << nl
+      code << assign(strobe, bus_if.strobe) << nl
+      code << assign(bus_if.ready, ready) << nl
+      code << assign(bus_if.status, "rggen_status'(#{status})") << nl
+      code << assign(bus_if.read_data, read_data) << nl
+    end
+  end
 end
--- spec/rggen/built_in/register/type/external_spec.rb
+++ spec/rggen/built_in/register/type/external_spec.rb
@@ -4,12 +4,17 @@ RSpec.describe 'register/type/external' do
   include_context 'clean-up builder'
   include_context 'register map common'
 
+  before(:all) do
+    RgGen.enable(:global, [:bus_width, :address_width])
+    RgGen.enable(:register, [:type, :size])
+    RgGen.enable(:register, :type, :external)
+    RgGen.enable(:bit_field, :name)
+  end
+
   describe 'register map' do
     before(:all) do
-      RgGen.enable(:global, [:bus_width, :address_width])
-      RgGen.enable(:register, [:type, :size])
-      RgGen.enable(:register, :type, :external)
-      RgGen.enable(:bit_field, :name)
+      delete_configuration_facotry
+      delete_register_map_factory
     end
 
     def create_registers(&block)
@@ -62,6 +67,284 @@ RSpec.describe 'register/type/external' do
           register { type :external; size [1, 1] }
         end
       }.to raise_register_map_error 'external register type supports single size definition only'
+    end
+  end
+
+  describe 'sv rtl' do
+    include_context 'sv rtl common'
+
+    before(:all) do
+      delete_configuration_facotry
+      delete_register_map_factory
+    end
+
+    before(:all) do
+      RgGen.enable(:global, :fold_sv_interface_port)
+      RgGen.enable(:register_block, [:name, :byte_size])
+      RgGen.enable(:register, [:name, :offset_address])
+      RgGen.enable(:register_block, :sv_rtl_top)
+      RgGen.enable(:register, :sv_rtl_top)
+    end
+
+    after(:all) do
+      RgGen.disable(:global, :fold_sv_interface_port)
+      RgGen.disable(:register_block, [:name, :byte_size])
+      RgGen.disable(:register, [:name, :offset_address])
+      RgGen.disable(:register_block, :sv_rtl_top)
+      RgGen.disable(:register, :sv_rtl_top)
+    end
+
+    def create_registers(fold_sv_interface_port, &body)
+      configuration = create_configuration(fold_sv_interface_port: fold_sv_interface_port)
+      create_sv_rtl(configuration, &body).registers
+    end
+
+    context 'fold_sv_interface_portが有効になっている場合' do
+      let(:register) do
+        registers = create_registers(true) do
+          name 'block_0'
+          byte_size 256
+          register { name 'register_0'; offset_address 0x00; type :external; size [1] }
+        end
+        registers.first
+      end
+
+      it 'インターフェースポート#bus_ifを持つ' do
+        expect(register)
+          .to have_interface_port :register_block, :bus_if, {
+            name: 'register_0_bus_if',
+            interface_type: 'rggen_bus_if',
+            modport: 'master'
+          }
+      end
+
+      specify '#bus_ifは個別ポートに展開されない' do
+        expect(register)
+          .to not_have_port :register_block, :valid, {
+            name: 'o_register_0_valid',
+            data_type: :logic,
+            direction: :output,
+            width: 1
+          }
+        expect(register)
+          .to not_have_port :register_block, :address, {
+            name: 'o_register_0_address',
+            data_type: :logic,
+            direction: :output,
+            width: 8
+          }
+        expect(register)
+          .to not_have_port :register_block, :write, {
+            name: 'o_register_0_write',
+            data_type: :logic,
+            direction: :output,
+            width: 1
+          }
+        expect(register)
+          .to not_have_port :register_block, :write_data, {
+            name: 'o_register_0_data',
+            data_type: :logic,
+            direction: :output,
+            width: 32
+          }
+        expect(register)
+          .to not_have_port :register_block, :strobe, {
+            name: 'o_register_0_strobe',
+            data_type: :logic,
+            direction: :output,
+            width: 4
+          }
+        expect(register)
+          .to not_have_port :register_block, :ready, {
+            name: 'i_register_0_ready',
+            data_type: :logic,
+            direction: :input,
+            width: 1
+          }
+        expect(register)
+          .to not_have_port :register_block, :status, {
+            name: 'i_register_0_status',
+            data_type: :logic,
+            direction: :input,
+            width: 2
+          }
+        expect(register)
+          .to not_have_port :register_block, :read_data, {
+            name: 'i_register_0_data',
+            data_type: :logic,
+            direction: :input,
+            width: 32
+          }
+      end
+    end
+
+    context 'fold_sv_interface_portが無効になっている場合' do
+      let(:register) do
+        registers = create_registers(false) do
+          name 'block_0'
+          byte_size 256
+          register { name 'register_0'; offset_address 0x00; type :external; size [1] }
+        end
+        registers.first
+      end
+
+      it '個別ポートに展開された#bus_ifを持つ' do
+        expect(register)
+          .to have_port :register_block, :valid, {
+            name: 'o_register_0_valid',
+            data_type: :logic,
+            direction: :output,
+            width: 1
+          }
+        expect(register)
+          .to have_port :register_block, :address, {
+            name: 'o_register_0_address',
+            data_type: :logic,
+            direction: :output,
+            width: 8
+          }
+        expect(register)
+          .to have_port :register_block, :write, {
+            name: 'o_register_0_write',
+            data_type: :logic,
+            direction: :output,
+            width: 1
+          }
+        expect(register)
+          .to have_port :register_block, :write_data, {
+            name: 'o_register_0_data',
+            data_type: :logic,
+            direction: :output,
+            width: 32
+          }
+        expect(register)
+          .to have_port :register_block, :strobe, {
+            name: 'o_register_0_strobe',
+            data_type: :logic,
+            direction: :output,
+            width: 4
+          }
+        expect(register)
+          .to have_port :register_block, :ready, {
+            name: 'i_register_0_ready',
+            data_type: :logic,
+            direction: :input,
+            width: 1
+          }
+        expect(register)
+          .to have_port :register_block, :status, {
+            name: 'i_register_0_status',
+            data_type: :logic,
+            direction: :input,
+            width: 2
+          }
+        expect(register)
+          .to have_port :register_block, :read_data, {
+            name: 'i_register_0_data',
+            data_type: :logic,
+            direction: :input,
+            width: 32
+          }
+      end
+
+      it 'rggen_bus_ifのインスタンスを持つ' do
+        expect(register)
+          .to have_interface :register, :bus_if, {
+            name: 'bus_if',
+            interface_type: 'rggen_bus_if',
+            parameter_values: [8, 32]
+          }
+      end
+    end
+
+    describe '#generate_code' do
+      it 'rggen_exernal_registerをインスタンスするコードを出力する' do
+        registers = create_registers(true) do
+          name 'block_0'
+          byte_size 256
+          register { name 'register_0'; offset_address 0x00; type :external; size [1] }
+          register { name 'register_1'; offset_address 0x80; type :external; size [32] }
+        end
+
+        expect(registers[0]).to generate_code(:register, :top_down, <<~'CODE')
+          rggen_external_register #(
+            .ADDRESS_WIDTH  (8),
+            .BUS_WIDTH      (32),
+            .START_ADDRESS  (8'h00),
+            .END_ADDRESS    (8'h03)
+          ) u_register (
+            .i_clk        (i_clk),
+            .i_rst_n      (i_rst_n),
+            .register_if  (register_if[0]),
+            .bus_if       (register_0_bus_if)
+          );
+        CODE
+
+        expect(registers[1]).to generate_code(:register, :top_down, <<~'CODE')
+          rggen_external_register #(
+            .ADDRESS_WIDTH  (8),
+            .BUS_WIDTH      (32),
+            .START_ADDRESS  (8'h80),
+            .END_ADDRESS    (8'hff)
+          ) u_register (
+            .i_clk        (i_clk),
+            .i_rst_n      (i_rst_n),
+            .register_if  (register_if[1]),
+            .bus_if       (register_1_bus_if)
+          );
+        CODE
+
+        registers = create_registers(false) do
+          name 'block_0'
+          byte_size 256
+          register { name 'register_0'; offset_address 0x00; type :external; size [1] }
+          register { name 'register_1'; offset_address 0x80; type :external; size [32] }
+        end
+
+        expect(registers[0]).to generate_code(:register, :top_down, <<~'CODE')
+          rggen_external_register #(
+            .ADDRESS_WIDTH  (8),
+            .BUS_WIDTH      (32),
+            .START_ADDRESS  (8'h00),
+            .END_ADDRESS    (8'h03)
+          ) u_register (
+            .i_clk        (i_clk),
+            .i_rst_n      (i_rst_n),
+            .register_if  (register_if[0]),
+            .bus_if       (bus_if)
+          );
+          assign o_register_0_valid = bus_if.valid;
+          assign o_register_0_address = bus_if.address;
+          assign o_register_0_write = bus_if.write;
+          assign o_register_0_data = bus_if.write_data;
+          assign o_register_0_strobe = bus_if.strobe;
+          assign bus_if.ready = i_register_0_ready;
+          assign bus_if.status = rggen_status'(i_register_0_status);
+          assign bus_if.read_data = i_register_0_data;
+        CODE
+
+        expect(registers[1]).to generate_code(:register, :top_down, <<~'CODE')
+          rggen_external_register #(
+            .ADDRESS_WIDTH  (8),
+            .BUS_WIDTH      (32),
+            .START_ADDRESS  (8'h80),
+            .END_ADDRESS    (8'hff)
+          ) u_register (
+            .i_clk        (i_clk),
+            .i_rst_n      (i_rst_n),
+            .register_if  (register_if[1]),
+            .bus_if       (bus_if)
+          );
+          assign o_register_1_valid = bus_if.valid;
+          assign o_register_1_address = bus_if.address;
+          assign o_register_1_write = bus_if.write;
+          assign o_register_1_data = bus_if.write_data;
+          assign o_register_1_strobe = bus_if.strobe;
+          assign bus_if.ready = i_register_1_ready;
+          assign bus_if.status = rggen_status'(i_register_1_status);
+          assign bus_if.read_data = i_register_1_data;
+        CODE
+      end
     end
   end
 end
