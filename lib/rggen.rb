b8dbec74330e1976534ab42cb3ee8b5260fb9d02
Taichi Ishitani <taichi730@gmail.com>
2020-08-16 00:20:34 +0900

    adapted for rggen/rggen-core#64

--- lib/rggen.rb
+++ lib/rggen.rb
@@ -1,4 +1,3 @@
 # frozen_string_literal: true
 
 require_relative 'rggen/version'
-require_relative 'rggen/default_plugins'
---
+++ lib/rggen/setup.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RgGen.load_plugin 'rggen-default-register-map'
+RgGen.load_plugin 'rggen-systemverilog/rtl'
+RgGen.load_plugin 'rggen-systemverilog/ral'
+RgGen.load_plugin 'rggen-markdown'
+RgGen.load_plugin 'rggen-spreadsheet-loader'
