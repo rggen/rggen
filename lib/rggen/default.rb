a66103d209cc577f317a9a04d670fce6c4b657f3
Taichi Ishitani <taichi730@gmail.com>
2025-01-18 01:13:18 +0900

    move `ignore_value` settings from the loader plugin
    (refs: rggen/rggen#235)

--- lib/rggen/default.rb
+++ lib/rggen/default.rb
@@ -6,3 +6,10 @@ RgGen.load_plugin 'rggen-systemverilog/ral'
 RgGen.load_plugin 'rggen-c-header'
 RgGen.load_plugin 'rggen-markdown'
 RgGen.load_plugin 'rggen-spreadsheet-loader'
+
+RgGen.update_plugin :'rggen-spreadsheet-loader' do |plugin|
+  plugin.setup_loader :register_map, :spreadsheet do |entry|
+    entry.ignore_value :register_block, :comment
+    entry.ignore_value :register, :comment
+  end
+end
